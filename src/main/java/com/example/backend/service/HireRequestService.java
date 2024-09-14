7b44526d84c8d178bc369add5231ec3a9096eb6a
Anjuluthra13 <122430165+Anjuluthra13@users.noreply.github.com>
2024-09-14 20:19:11 +0530

    deploy

--- src/main/java/com/example/backend/service/HireRequestService.java
+++ src/main/java/com/example/backend/service/HireRequestService.java
@@ -1,13 +1,12 @@
 package com.example.backend.service;
 
-
 import com.example.backend.model.HireRequest;
 import com.example.backend.repository.HireRequestRepository;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class HireRequestService {
@@ -15,10 +14,12 @@ public class HireRequestService {
     @Autowired
     private HireRequestRepository hireRequestRepository;
 
+    // Save each hire request
     public HireRequest saveHireRequest(HireRequest hireRequest) {
         return hireRequestRepository.save(hireRequest);
     }
-    // New method to fetch all hire requests
+
+    // Retrieve all hire requests
     public List<HireRequest> getAllHireRequests() {
         return hireRequestRepository.findAll();
     }
